585404af77115f2b6be2080eafedf251e8c3b776
thrillroyd <82989204+thrillroyd@users.noreply.github.com>
2021-09-26 09:36:33 +0700

    can use with mongoose

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash')
+const _ = require("lodash");
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/blogDb');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,53 +13,79 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-var posts = []
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('home', { homeStartingContent: homeStartingContent, posts: posts })
+// Setting Mongodb 
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String
 })
+const Blog = mongoose.model('Blog', blogSchema)
 
-app.get('/contact', (req, res) => {
-    res.render('contact', { contactContent: contactContent })
-})
+let posts = [];
 
-app.get('/about', (req, res) => {
-    res.render('about', { aboutContent: aboutContent })
-})
+app.get("/", function(req, res) {
+    let posts = []
+        // bring data from database
+    Blog.find({}, (err, blogs) => {
+        blogs.forEach((elem) => {
+            posts.push(elem)
+        })
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts
+        })
+    })
+});
 
-app.get('/compose', (req, res) => {
-    res.render('compose')
-})
+app.get("/about", function(req, res) {
+    res.render("about", { aboutContent: aboutContent });
+});
 
-app.post('/compose', (req, res) => {
-    const post = {
-        title: req.body.blogTitle,
-        content: req.body.blogContent
-    }
-    posts.push(post)
-    res.redirect('/')
-})
+app.get("/contact", function(req, res) {
+    res.render("contact", { contactContent: contactContent });
+});
 
-app.get('/posts/:title', (req, res) => {
-    const titles = []
-    posts.forEach((element) => titles.push(_.lowerCase(element.title)))
-    console.log(titles)
-    if (titles.includes(_.lowerCase(req.params.title))) {
-        console.log('Match Found!')
-        const showIndex = titles.indexOf(_.lowerCase(req.params.title))
-        res.render('post', { postTitle: posts[showIndex].title, postContent: posts[showIndex].content })
-    } else {
-        console.log('Not a Match!')
-    }
-})
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
 
+app.post("/compose", function(req, res) {
+    const post = new Blog({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    })
 
+    post.save()
+    res.redirect("/");
 
+});
+
+app.get("/posts/:post_id", function(req, res) {
+    const postId = req.params.post_id
+    console.log(req.params)
+    console.log(postId)
+
+    Blog.findOne({ _id: postId }, (err, foundBlog) => {
+            console.log(foundBlog)
+            res.render('post', { title: foundBlog.title, content: foundBlog.content })
+        })
+        // const requestedTitle = _.lowerCase(req.params.post_id]);
+
+    // posts.forEach(function(post) {
+    //     const storedTitle = _.lowerCase(post.title);
+
+    //     if (storedTitle === requestedTitle) {
+    //         res.render("post", {
+    //             title: post.title,
+    //             content: post.content
+    //         });
+    //     }
+    // });
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
